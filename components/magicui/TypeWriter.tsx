c731f605013095cf219db94011d27ac80fe0c2e0
Krushant <krushantw@gmail.com>
2025-04-25 19:23:09 +0530

    Implemented some changes in UI

--- components/magicui/TypeWriter.tsx
+++ components/magicui/TypeWriter.tsx
@@ -18,9 +18,10 @@ export function TypewriterEffectSmoothDemo() {
       <TypewriterEffectSmooth words={words} className="text-white" />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
         <Link href="https://drive.google.com/file/d/1DMdBfKIm_AizjX5qcfhoJApcGYEEUuBF/view?usp=sharing">
-        <button className="w-40 h-10 rounded-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-600 hover:via-purple-600 hover:to-pink-600 text-white text-sm transition-all duration-300">
-           Download CV
-        </button>
+        <button className="w-40 h-10 rounded-xl bg-blue-700 hover:bg-blue-800 text-white text-sm transition-all duration-300">
+  Download CV
+</button>
+
         </Link>
         <Link href="#contact">
           <button className="w-40 h-10 rounded-xl bg-white text-black hover:bg-gray-400 border border-black  text-sm">
