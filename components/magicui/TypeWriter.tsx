f5785b979b751613c83142ff8b4c2fd3a7ef7ec4
Krushant <krushantw@gmail.com>
2025-04-25 19:10:21 +0530

    Implemented changes in CV

--- components/magicui/TypeWriter.tsx
+++ components/magicui/TypeWriter.tsx
@@ -17,7 +17,7 @@ export function TypewriterEffectSmoothDemo() {
     <div className="flex flex-col items-center justify-center h-[10rem]  ">
       <TypewriterEffectSmooth words={words} className="text-white" />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <Link href="https://drive.google.com/file/d/1FCMLk29jLCVI8ufRpPNRGV51LuGdafpQ/view?usp=sharing">
+        <Link href="https://drive.google.com/file/d/1DMdBfKIm_AizjX5qcfhoJApcGYEEUuBF/view?usp=sharing">
         <button className="w-40 h-10 rounded-xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-indigo-600 hover:via-purple-600 hover:to-pink-600 text-white text-sm transition-all duration-300">
            Download CV
         </button>
